99e26f67bcd10d6dc253b11610bacf7bcd4a50e5
Salem Ribeiro <salem.junior@geru.com.br>
2021-03-11 14:53:34 -0300

    inicial sources

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int n_bytes;
+
+    n_bytes = value;
+}
---
+++ src/memall.h
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef MEMMALL_H
+
+int     alloc_memory(char value);
+
+#endif
